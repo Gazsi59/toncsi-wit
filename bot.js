ec8b37e60e79bb2f9f0a22623c6e76092a93d53c
Gazsi59 <gaspar.sandor.59@gmail.com>
2017-01-22 15:08:50 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -195,19 +195,24 @@ console.log(body) // Print the json response
 // honap+":"+${day.getDate()} Min:${JSON.stringify(retData.list[q].temp.min)} Max:${JSON.stringify(retData.list[q].temp.max)} ${retData.list[q].weather[0].description}`
 // 				day.setDate(day.getDate()+1);
 
-//				if (q = 0)
-//				{context.forecast =  context.forecast +
-//				`Ma  Min:${JSON.stringify(retData.list[qq].temp.min)} Max:${JSON.stringify(retData.list[qq].temp.max)} ${retData.list[qq].weather[0].description}`
-//				}
+				if (q = 0)
+				{context.forecast =  context.forecast +
+				`Ma  Min:${JSON.stringify(retData.list[qq].temp.min)} Max:${JSON.stringify(retData.list[qq].temp.max)} ${retData.list[qq].weather[0].description}
+`
+				}
+				else
+				{context.forecast =  context.forecast +
+				`${day.getMonth()} ${day.getDate()} Min:${JSON.stringify(retData.list[q].temp.min)} Max:${JSON.stringify(retData.list[q].temp.max)} ${retData.list[q].weather[0].description}
+`
 				
 				
 //				`Ma  Min:${JSON.stringify(retData.list[qq].temp.min)} Max:${JSON.stringify(retData.list[qq].temp.max)} ${retData.list[qq].weather[0].description}`
 //				if (q < 0)
 //
 				
-				{context.forecast =  context.forecast +
-				`${day.getMonth()} ${day.getDate()} Min:${JSON.stringify(retData.list[q].temp.min)} Max:${JSON.stringify(retData.list[q].temp.max)} ${retData.list[q].weather[0].description}
-`
+//				{context.forecast =  context.forecast +
+//				`${day.getMonth()} ${day.getDate()} Min:${JSON.stringify(retData.list[q].temp.min)} Max:${JSON.stringify(retData.list[q].temp.max)} ${retData.list[q].weather[0].description}
+//`
 				}
 q = q + 1;
 				day.setDate(day.getDate()+1);
