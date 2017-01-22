cc43c39f8a8fd1427344ec9393673dab038c6f21
Gazsi59 <gaspar.sandor.59@gmail.com>
2017-01-22 16:12:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -207,14 +207,13 @@ console.log(body) // Print the json response
 				if (qq < '0 0')
 				{context.forecast =  context.forecast +
 				`Ma:  QQ = ${qq} Min:${JSON.stringify(retData.list[qq].temp.min)} Max:${JSON.stringify(retData.list[qq].temp.max)} 
-       ${retData.list[qq].weather[0].description}
+        ${retData.list[qq].weather[0].description}
 `
 				}
 				else
 				{context.forecast =  context.forecast +
-				` QQ = ${qq} ${honap} ${day.getMonth()} ${day.getDate()} 
-Min:${JSON.stringify(retData.list[qq].temp.min)} Max:${JSON.stringify(retData.list[qq].temp.max)} 
-       ${retData.list[qq].weather[0].description}
+				` QQ = ${qq} ${honap}  ${day.getDate()} Min:${JSON.stringify(retData.list[qq].temp.min)} Max:${JSON.stringify(retData.list[qq].temp.max)} 
+        ${retData.list[qq].weather[0].description}
 `
 				}
 				day.setDate(day.getDate()+1);
