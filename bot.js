e4ee9f4bce6764ffbed69f50510b86dd6487f347
Gazsi59 <gaspar.sandor.59@gmail.com>
2017-01-22 19:50:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -224,7 +224,7 @@ console.log(body) // Print the json response
 				}
 				else
 				{context.forecast =  context.forecast +
-				`${qq}.${day.getDate()}: Min:${JSON.stringify(retData.list[qq].temp.min)} Max:${JSON.stringify(retData.list[qq].temp.max)} 
+				`${substr(qq,3)}.${day.getDate()}: Min:${JSON.stringify(retData.list[qq].temp.min)} Max:${JSON.stringify(retData.list[qq].temp.max)} 
 	${retData.list[qq].weather[0].description}
 `
 				}
