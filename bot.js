8fc0f406862f3f4c3e81dd4c61e5ebae333c86be
Gazsi59 <gaspar.sandor.59@gmail.com>
2017-01-22 15:30:47 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -194,12 +194,12 @@ console.log(body) // Print the json response
 // honap+":"+${day.getDate()} Min:${JSON.stringify(retData.list[q].temp.min)} Max:${JSON.stringify(retData.list[q].temp.max)} ${retData.list[q].weather[0].description}`
 // 				day.setDate(day.getDate()+1);
 
-				if (q = '0')
+				if (q < '0')
 				{context.forecast =  context.forecast +
 				`Ma: Min:${JSON.stringify(retData.list[qq].temp.min)} Max:${JSON.stringify(retData.list[qq].temp.max)} 
     ${retData.list[qq].weather[0].description}
 `
-				q = '1';
+				
 				}
 				else
 				{context.forecast =  context.forecast +
