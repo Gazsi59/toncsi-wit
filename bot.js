51d48c33c59269e4a14eefb832ee018f82504050
Gazsi59 <gaspar.sandor.59@gmail.com>
2017-01-22 16:01:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -179,7 +179,7 @@ console.log(body) // Print the json response
  			for( qq in retData.list)
 			{
 
- if (  substr(3).${day.getMonth()  }  = '0' )   { honap = "Jan"; }
+ if (  substr(3).qq   = '0' )   { honap = "Jan"; }
 // if ( ${day.getMonth()+1} = "2")   { honap = "Feb"; }
 // if ( ${day.getMonth()+1} = "3")   { honap = "Mar"; }
 // if ( ${day.getMonth()+1} = "4")   { honap = "Apr"; }
