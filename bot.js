7507827feb224b8395f2ed7125086e81df5e8d22
Gazsi59 <gaspar.sandor.59@gmail.com>
2017-01-22 20:29:31 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -202,7 +202,7 @@ console.log(body) // Print the json response
 // getForecast bot executes
   ['getForecast'](sessionId, context, cb) {
 	   request(
-		 `http://api.openweathermap.org/data/2.5/forecast/daily?q=${context.loc}&units=metric&lang=hu&cnt=10&APPID=07976ea0d7f1371a9e527add86391b84`, get7Day); 	   function get7Day(err, response, body)
+		 `http://api.openweathermap.org/data/2.5/forecast/daily?q=${context.loc}&units=metric&lang=hu&cnt=15&APPID=07976ea0d7f1371a9e527add86391b84`, get7Day); 	   function get7Day(err, response, body)
 	  {
 			var retData = JSON.parse(body);
 			var params = [];
